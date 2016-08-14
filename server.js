690cdc5600f5a79e3a595a4973d1b8804721c819
2remdou <2remdou@gmail.com>
2016-08-14 13:30:24 +0000

    integre jackrabbit

--- server.js
+++ server.js
@@ -3,5 +3,31 @@
 const express = require('express');
 const app = express();
 
+const jackrabbit = require('jackrabbit');
+const rabbit = jackrabbit(process.env.RABBITURL);
+const logger = require('logfmt');
+
+var exchange = rabbit.direct('notification');
+var ios = exchange.queue({name: 'ios',key: 'ios' });
+var android = exchange.queue({name: 'android',key: 'android'});
+
+var apn = require('./services/ApnsNotification');
+var gcm = require('./services/GcmNotification');
+
+ios.consume(consumeIos);
+android.consume(consumeAndroid);
+
+function consumeIos(data,ack){
+    console.log('push ios %s on %s',data.content,data.token);
+    apn.push(data);
+    ack();
+};
+
+function consumeAndroid(data,ack){
+    console.log('push android %s on %s',data.content,data.token);
+    gcm.push(data);
+    ack();
+}
+
 
 app.listen(process.env.PORT || 5000);
